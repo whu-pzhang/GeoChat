c7ba340bb7a619379cdb2cb368ee7f0c3d5644a1
whu-pzhang <pzhang@whu.edu.cn>
2024-11-05 11:14:47 +0800

    update

--- geochat/model/multimodal_encoder/clip_encoder.py
+++ geochat/model/multimodal_encoder/clip_encoder.py
@@ -2,12 +2,14 @@ import torch
 import torch.nn as nn
 import math
 from PIL import ImageFile
+
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 from transformers import CLIPVisionModel, CLIPImageProcessor, CLIPVisionConfig
 
 
 class CLIPVisionTower(nn.Module):
-    def clip_interpolate_embeddings(self, image_size=600, patch_size= 14):
+
+    def clip_interpolate_embeddings(self, image_size=600, patch_size=14):
         """This function helps interpolating positional embeddings during checkpoint loading,
         especially when you want to apply a pre-trained model on images with different resolution.
  
@@ -22,15 +24,17 @@ class CLIPVisionTower(nn.Module):
             OrderedDict[str, torch.Tensor]: A state dict which can be loaded into the new model.
         """
         # Shape of pos_embedding is (1, seq_length, hidden_dim)
-        state_dict = self.vision_tower.vision_model.embeddings.position_embedding.state_dict()
+        state_dict = self.vision_tower.vision_model.embeddings.position_embedding.state_dict(
+        )
         pos_embedding = state_dict['weight']
         pos_embedding = pos_embedding.unsqueeze(0)
         n, seq_length, hidden_dim = pos_embedding.shape
         if n != 1:
-            raise ValueError(f"Unexpected position embedding shape: {pos_embedding.shape}")
- 
-        new_seq_length = (image_size // patch_size) ** 2 + 1
- 
+            raise ValueError(
+                f"Unexpected position embedding shape: {pos_embedding.shape}")
+
+        new_seq_length = (image_size // patch_size)**2 + 1
+
         # Need to interpolate the weights for the position embedding.
         # We do this by reshaping the positions embeddings to a 2d grid, performing
         # an interpolation in the (h, w) space and then reshaping back to a 1d grid.
@@ -40,16 +44,18 @@ class CLIPVisionTower(nn.Module):
             new_seq_length -= 1
             pos_embedding_token = pos_embedding[:, :1, :]
             pos_embedding_img = pos_embedding[:, 1:, :]
- 
+
             # (1, seq_length, hidden_dim) -> (1, hidden_dim, seq_length)
             pos_embedding_img = pos_embedding_img.permute(0, 2, 1)
             seq_length_1d = int(math.sqrt(seq_length))
-            torch._assert(seq_length_1d * seq_length_1d == seq_length, "seq_length is not a perfect square!")
- 
+            torch._assert(seq_length_1d * seq_length_1d == seq_length,
+                          "seq_length is not a perfect square!")
+
             # (1, hidden_dim, seq_length) -> (1, hidden_dim, seq_l_1d, seq_l_1d)
-            pos_embedding_img = pos_embedding_img.reshape(1, hidden_dim, seq_length_1d, seq_length_1d)
+            pos_embedding_img = pos_embedding_img.reshape(
+                1, hidden_dim, seq_length_1d, seq_length_1d)
             new_seq_length_1d = image_size // patch_size
- 
+
             # Perform interpolation.
             # (1, hidden_dim, seq_l_1d, seq_l_1d) -> (1, hidden_dim, new_seq_l_1d, new_seq_l_1d)
             new_pos_embedding_img = nn.functional.interpolate(
@@ -58,20 +64,25 @@ class CLIPVisionTower(nn.Module):
                 mode='bicubic',
                 align_corners=True,
             )
- 
+
             # (1, hidden_dim, new_seq_l_1d, new_seq_l_1d) -> (1, hidden_dim, new_seq_length)
-            new_pos_embedding_img = new_pos_embedding_img.reshape(1, hidden_dim, new_seq_length)
- 
+            new_pos_embedding_img = new_pos_embedding_img.reshape(
+                1, hidden_dim, new_seq_length)
+
             # (1, hidden_dim, new_seq_length) -> (1, new_seq_length, hidden_dim)
             new_pos_embedding_img = new_pos_embedding_img.permute(0, 2, 1)
-            new_pos_embedding = torch.cat([pos_embedding_token, new_pos_embedding_img], dim=1)[0]
+            new_pos_embedding = torch.cat(
+                [pos_embedding_token, new_pos_embedding_img], dim=1)[0]
             state_dict['weight'] = new_pos_embedding
-            self.vision_tower.vision_model.embeddings.position_embedding = nn.Embedding(new_seq_length+1, hidden_dim)
-            self.vision_tower.vision_model.embeddings.position_embedding.load_state_dict(state_dict)
+            self.vision_tower.vision_model.embeddings.position_embedding = nn.Embedding(
+                new_seq_length + 1, hidden_dim)
+            self.vision_tower.vision_model.embeddings.position_embedding.load_state_dict(
+                state_dict)
             self.vision_tower.vision_model.embeddings.image_size = image_size
             self.vision_tower.vision_model.embeddings.patch_size = patch_size
-            self.vision_tower.vision_model.embeddings.position_ids = torch.arange(new_seq_length+1).expand((1, -1))
-            
+            self.vision_tower.vision_model.embeddings.position_ids = torch.arange(
+                new_seq_length + 1).expand((1, -1))
+
     def __init__(self, vision_tower, args, delay_load=False):
         super().__init__()
 
@@ -79,20 +90,26 @@ class CLIPVisionTower(nn.Module):
 
         self.vision_tower_name = vision_tower
         self.select_layer = args.mm_vision_select_layer
-        self.select_feature = getattr(args, 'mm_vision_select_feature', 'patch')
+        self.select_feature = getattr(args, 'mm_vision_select_feature',
+                                      'patch')
 
         if not delay_load:
             self.load_model()
         else:
-            self.cfg_only = CLIPVisionConfig.from_pretrained(self.vision_tower_name)
-            self.image_processor = CLIPImageProcessor.from_pretrained(self.vision_tower_name)
-            self.vision_tower = CLIPVisionModel.from_pretrained(self.vision_tower_name)
-            self.vision_tower.requires_grad_(False)
+            self.cfg_only = CLIPVisionConfig.from_pretrained(
+                self.vision_tower_name)
+            # self.image_processor = CLIPImageProcessor.from_pretrained(
+            #     self.vision_tower_name)
+            # self.vision_tower = CLIPVisionModel.from_pretrained(
+            #     self.vision_tower_name)
+            # self.vision_tower.requires_grad_(False)
             self.clip_interpolate_embeddings(image_size=504, patch_size=14)
 
     def load_model(self):
-        self.image_processor = CLIPImageProcessor.from_pretrained(self.vision_tower_name)
-        self.vision_tower = CLIPVisionModel.from_pretrained(self.vision_tower_name)
+        self.image_processor = CLIPImageProcessor.from_pretrained(
+            self.vision_tower_name)
+        self.vision_tower = CLIPVisionModel.from_pretrained(
+            self.vision_tower_name)
         self.vision_tower.requires_grad_(False)
         self.clip_interpolate_embeddings(image_size=504, patch_size=14)
 
@@ -106,7 +123,8 @@ class CLIPVisionTower(nn.Module):
         elif self.select_feature == 'cls_patch':
             image_features = image_features
         else:
-            raise ValueError(f'Unexpected select feature: {self.select_feature}')
+            raise ValueError(
+                f'Unexpected select feature: {self.select_feature}')
         return image_features
 
     @torch.no_grad()
@@ -116,25 +134,34 @@ class CLIPVisionTower(nn.Module):
             for image in images:
                 # print(image.shape)
                 # import pdb; pdb.set_trace()
-                image_forward_out = self.vision_tower(image.to(device=self.device, dtype=self.dtype).unsqueeze(0), output_hidden_states=True)
-                
-                image_feature = self.feature_select(image_forward_out).to(image.dtype)
+                image_forward_out = self.vision_tower(
+                    image.to(device=self.device,
+                             dtype=self.dtype).unsqueeze(0),
+                    output_hidden_states=True)
+
+                image_feature = self.feature_select(image_forward_out).to(
+                    image.dtype)
                 # print(image_features.shape)
-                
+
                 image_features.append(image_feature)
         else:
             # print(images.shape)
             # import pdb; pdb.set_trace()
-            image_forward_outs = self.vision_tower(images.to(device=self.device, dtype=self.dtype), output_hidden_states=True)
-            image_features = self.feature_select(image_forward_outs).to(images.dtype)
+            image_forward_outs = self.vision_tower(images.to(
+                device=self.device, dtype=self.dtype),
+                                                   output_hidden_states=True)
+            image_features = self.feature_select(image_forward_outs).to(
+                images.dtype)
             # print(image_features.shape)
-            
 
         return image_features
 
     @property
     def dummy_feature(self):
-        return torch.zeros(1, self.hidden_size, device=self.device, dtype=self.dtype)
+        return torch.zeros(1,
+                           self.hidden_size,
+                           device=self.device,
+                           dtype=self.dtype)
 
     @property
     def dtype(self):
@@ -157,4 +184,4 @@ class CLIPVisionTower(nn.Module):
 
     @property
     def num_patches(self):
-        return (self.config.image_size // self.config.patch_size) ** 2
+        return (self.config.image_size // self.config.patch_size)**2
